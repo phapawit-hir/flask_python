b943dbecfb5b618cfe9d4c71fbea021b3bbecad8
Phapawit Hirunsahitphon <phapawit-hir@github.com>
2023-06-06 14:18:06 +0700

    test

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+@app.route('/Doraemon')
+def hello():
+    return 'Doraemon page!!!!'
 
 if __name__ =='__main__':
     app.run(debug = True, host='0.0.0.0', port='3333')
